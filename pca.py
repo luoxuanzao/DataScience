6bf2b666904c0b4a40b9d823efa4daa82c2c69a8
山山 <181250019@smail.nju.edu.cn>
2020-07-24 15:21:26 +0800

    add: 研究报告

--- pca.py
+++ pca.py
@@ -17,7 +17,29 @@ pyPercent = json.loads(res)
 matrix = []
 for k in lines.keys():
     matrix.append([lines[k], marks[k], pyPercent[k]])
+
+# 标准化
+average = []
+distance = []
+for i in range(3):
+    minI = 0
+    maxI = 1000
+    total = 0
+    for k in range(len(matrix)):
+        minI = min(minI, matrix[k][i])
+        maxI = max(maxI, matrix[k][i])
+        total += matrix[k][i]
+    distance.append(maxI-minI)
+    average.append(total/len(matrix))
+
+for i in range(3):
+    for k in range(len(matrix)):
+        matrix[k][i] = (matrix[k][i] - average[i])/distance[i]
+print(matrix)
+
 T = np.array(matrix)
-pca = PCA(n_components=3)
+pca = PCA(n_components=3, whiten=True)
 pca.fit(T)
+print("行数 分数 py占比")
 print(pca.explained_variance_ratio_)
+
